c1bba2c6c71d89ec7117190c646433d29f20ab3b
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2024-09-27 21:24:25 -0400

    finish assignment 3

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    surname_1 = "Mykytyshyn"
+    surname_2 = "Krasovskyy"
+    surname_3 = "Hromyak"
+
+    print(f"This is the project of {surname_1}, {surname_2} and {surname_3}")
