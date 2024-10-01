59feffe6e0d99c66f4c29b0b110b35c33f62f23e
DoctorKaufman <andrey.krasovskyy@gmail.com>
2024-10-01 13:06:52 +0300

    Added guard for main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("This is the project of Krasovskyy, Hromiak and Mykytyshyn.")
+
+def project_hello():
+    print("This is the project of Krasovskyy, Hromiak and Mykytyshyn.")
+
+if __name__ == "__main__":
+    project_hello()
