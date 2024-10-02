ebf8cf728a6f5585956c0fb2fd58a0e24e486777
DoctorKaufman <andrey.krasovskyy@gmail.com>
2024-10-02 19:05:35 +0300

    Added spark session creation in main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def project_hello():
-    print("This is the project of Krasovskyy, Hromiak and Mykytyshyn.")
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+def create_spark_session():
+    """
+    Creates and returns Spark session
+
+    Returns: SparkSession object
+    """
+    spark_conf = SparkConf()
+    spark = (SparkSession.builder
+             .master("local")
+             .appName("pyspark project")
+             .config(conf=spark_conf)
+             .getOrCreate())
+    return spark
 
 if __name__ == "__main__":
-    project_hello()
+    spark_session = create_spark_session()
