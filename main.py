c98bb56f57942dd3b5950a71aeecf9648ac2e9f1
OlexiyHR <groleks30@gmail.com>
2024-10-14 23:38:11 +0300

    added processing of trip_data

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pyspark.sql import SparkSession
 from basic_dfs import basic_df_Krasovskyy as basic_df_k
 from basic_dfs.basic_df_mykytyshyn import basic_test_df as basic_test_df_myk
 from basic_dfs.basic_df_Hromiak import basic_test_df as basic_test_df_Hromiak
+from read_write import read_fare_data_df, write_fare_data_df_to_csv, read_trip_data_df, write_trip_data_df_to_csv
+from settings import TRIP_FARE_READ_DIRECTORY_PATH, TRIP_FARE_WRITE_DIRECTORY_PATH, TRIP_DATA_READ_DIRECTORY_PATH, TRIP_DATA_WRITE_DIRECTORY_PATH, WRITE_PARTITION
 
 
 def create_spark_session():
@@ -32,7 +34,7 @@ def display_demo_dataframe_krasovskyy():
 
 def display_demo_dataframe_Hromiak():
     df = basic_test_df_Hromiak(spark_session)
-    df.show()
+
 
 if __name__ == "__main__":
     spark_session = create_spark_session()
@@ -40,5 +42,41 @@ if __name__ == "__main__":
     display_demo_dataframe_krasovskyy()
     display_demo_dataframe_mykytyshyn()
     display_demo_dataframe_Hromiak()
+
+    fare_data_df = read_fare_data_df(
+        spark_session=spark_session,
+        dataframe_path=TRIP_FARE_READ_DIRECTORY_PATH,
+        header=True,
+        sep=",",
+        null_value="NULL",
+        mode="FAILFAST",
+        multi_line=True
+    )
+
+    write_fare_data_df_to_csv(
+        df=fare_data_df,
+        write_folder_path=TRIP_FARE_WRITE_DIRECTORY_PATH,
+        num_files=WRITE_PARTITION,
+        header=True,
+        sep=","
+    )
+
+    trip_data_df = read_trip_data_df(
+        spark_session=spark_session,
+        dataframe_path=TRIP_DATA_READ_DIRECTORY_PATH,
+        header=True,
+        sep=",",
+        null_value="NULL",
+        mode="FAILFAST",
+        multi_line=True
+    )
+
+    write_trip_data_df_to_csv(
+        df=trip_data_df,
+        write_folder_path=TRIP_DATA_WRITE_DIRECTORY_PATH,
+        num_files=WRITE_PARTITION,
+        header=True,
+        sep=","
+    )
 
     spark_session.stop()
