a38466ba42ab2e04cb5d32a1a15a1e601c75cf26
DoctorKaufman <andrey.krasovskyy@gmail.com>
2024-10-28 20:36:47 +0200

    Added count_cash_tips_above_average() (question 22).

--- data_analysis/fare_data_analysis.py
+++ data_analysis/fare_data_analysis.py
@@ -1,6 +1,7 @@
 from pyspark.sql import functions as f
 import columns as c
 
+
 def count_evening_rides_with_high_total_amount(df):
     """
     Count trips that took place in the evening (between 18:00 and 23:59) and
@@ -10,7 +11,7 @@ def count_evening_rides_with_high_total_amount(df):
         Question 21.
 
     Args:
-        df (DataFrame): Trip fare data DataFrame to process.
+        df (DataFrame): Fare data DataFrame to process.
 
     Returns:
         int: Count of evening trips with a total_amount above mean value .
@@ -26,3 +27,26 @@ def count_evening_rides_with_high_total_amount(df):
     ).count()
 
     return evening_trips_with_high_total_amount_count
+
+
+def count_cash_tips_above_average(df):
+    """
+    Count cash-paid trips that have tip amount above mean value of tip amount.
+
+    Args:
+        df (DataFrame): Fare data DataFrame to process.
+
+    Returns:
+        int: Count of cash-paid trips with `tip_amount` above the average.
+
+    Examples:
+        >>> cash_tips_above_average_count = count_cash_tips_above_average(df)
+    """
+    tip_amount_mean = df.select(f.avg(f.col(c.tip_amount))).first()[0]
+
+    cash_tip_count = df.where(
+        (f.col(c.payment_type) == "CSH")
+        & (f.col(c.tip_amount) > tip_amount_mean)
+    ).count()
+
+    return cash_tip_count
